metti/libabigail

commit 6a56bcb744e83150f545ecf0a9b6dbbee9aa8348

symtab: Add support for MODVERSIONS (CRC checksums)
The Linux Kernel has a mechanism (MODVERSIONS) to checksum symbols based
on their type. In a way similar to what libabigail does, but different.
The CRC values for symbols can be extracted from the symtab either by
following the __kcrctab_<symbol> entry or by using the __crc_<symbol>
value directly.

This patch adds support for extracting those CRC values and storing them
as a property of elf_symbol. Subsequently, 'crc' gets emitted as an
attribute of 'elf-symbol' in the XML representation.

CRC comparisons are also added to the abidiff machinery such that if
both representations of a comparison contain a CRC value, they will be
compared and if any of the values is unset (i.e. == 0), equality is
assumed. Differences will be reported in the format that the Kernel
presents them e.g. via Module.symvers. It is likely, but not necessary,
that a CRC difference comes along with an ABI difference reported by
libabigail. Not everything that leads to a change of the CRC value an
ABI breakage in the libabigail sense.

Also add some test cases to ensure reading crc values from kernel
binaries works as expected. The empty-report files have been
consolidated to one file: empty-report.txt. That also clarifies the
expected outcome for the affected tests.

	* include/abg-ir.h (elf_symbol::elf_symbol): Add crc parameter.
	(elf_symbol::create): Likewise.
	(elf_symbol::get_crc): New member method.
	(elf_symbol::set_crc): New member method.
	* src/abg-ir.cc (elf_symbol::priv::crc_): New data member.
	* src/abg-ir.cc (elf_symbol::priv::priv): Add crc parameter.
	(elf_symbol::elf_symbol): Likewise.
	(elf_symbol::create): Likewise.
	(elf_symbol::textually_equals): Add crc support.
	(elf_symbol::get_crc): New member method.
	(elf_symbol::set_crc): New member method.
	* src/abg-reader.cc (build_elf_symbol): Add crc support.
	* src/abg-reporter-priv.cc (maybe_report_diff_for_symbol): Likewise.

patches/series
@@ -23,3 +23,4 @@ fixup-dwarf-reader-drop-now-unused-code-related-symbol-table-reading.patch
 test-symtab-add-tests-for-whitelisted-functions.patch
 symtab-dwarf-reader-allow-hinting-of-main-symbols-for-aliases.patch
 dwarf-reader-writer-consider-aliases-when-dealing-with-suppressions.patch
+symtab-Add-support-for-MODVERSIONS-CRC-checksums.patch

tests/data/Makefile.am
@@ -35,6 +35,7 @@ test-read-write/test28-without-std-fns-ref.xml \
 test-read-write/test28-drop-std-vars.abignore \
 test-read-write/test28-without-std-vars-ref.xml \
 test-read-write/test28-without-std-vars.xml \
+test-read-write/test-crc.xml \
 \
 test-write-read-archive/test0.xml \
 test-write-read-archive/test1.xml \
@@ -57,6 +58,7 @@ test-core-diff/report11.txt \
 test-core-diff/report12.txt \
 test-core-diff/report13.txt \
 \
+test-abidiff/empty-report.txt			\
 test-abidiff/test-enum0-v0.cc.bi		\
 test-abidiff/test-enum0-v1.cc.bi		\
 test-abidiff/test-enum0-report.txt		\
@@ -77,17 +79,18 @@ test-abidiff/test-var0-v1.cc.bi		\
 test-abidiff/test-var0-report.txt	\
 test-abidiff/test-PR18166-libtirpc.so \
 test-abidiff/test-PR18166-libtirpc.so.abi \
-test-abidiff/test-PR18166-libtirpc.so.report.txt \
 test-abidiff/test-PR18791-report0.txt   \
 test-abidiff/test-PR18791-v0.so.abi     \
 test-abidiff/test-PR18791-v1.so.abi     \
-test-abidiff/test-PR24552-report0.txt	\
 test-abidiff/test-PR24552-v0.abi	\
 test-abidiff/test-PR24552-v1.abi	\
 test-abidiff/test-empty-corpus-0.xml		\
 test-abidiff/test-empty-corpus-1.xml		\
 test-abidiff/test-empty-corpus-2.xml		\
-test-abidiff/test-empty-corpus-report.txt	\
+test-abidiff/test-crc-0.xml \
+test-abidiff/test-crc-1.xml \
+test-abidiff/test-crc-2.xml \
+test-abidiff/test-crc-report.txt \
 \
 test-abidiff-exit/test1-voffset-change-report0.txt \
 test-abidiff-exit/test1-voffset-change-report1.txt \
@@ -1852,4 +1855,7 @@ test-symtab/kernel-5.6/single_function_gpl.ko \
 test-symtab/kernel-5.6/single_variable.c \
 test-symtab/kernel-5.6/single_variable.ko \
 test-symtab/kernel-5.6/single_variable_gpl.c \
-test-symtab/kernel-5.6/single_variable_gpl.ko
+test-symtab/kernel-5.6/single_variable_gpl.ko \
+test-symtab/kernel-modversions/Makefile \
+test-symtab/kernel-modversions/one_of_each.c \
+test-symtab/kernel-modversions/one_of_each.ko

include/abg-ir.h
@@ -854,6 +854,7 @@ private:
 	     visibility		vi,
 	     bool		is_linux_string_cst = false,
 	     bool		is_in_ksymtab = false,
+	     uint64_t		crc = 0,
 	     bool		is_suppressed = false);
 
   elf_symbol(const elf_symbol&);
@@ -879,6 +880,7 @@ public:
 	 visibility	    vi,
 	 bool		    is_linux_string_cst = false,
 	 bool		    is_in_ksymtab = false,
+	 uint64_t	    crc = 0,
 	 bool		    is_suppressed = false);
 
   const environment*
@@ -952,6 +954,12 @@ public:
 
   void
   set_is_in_ksymtab(bool is_in_ksymtab);
+
+  uint64_t
+  get_crc() const;
+
+  void
+  set_crc(uint64_t crc);
 
   bool
   is_suppressed() const;

src/abg-ir.cc
@@ -24,13 +24,14 @@
 ///
 /// Definitions for the Internal Representation artifacts of libabigail.
 
-#include <cxxabi.h>
-#include <vector>
-#include <utility>
 #include <algorithm>
+#include <cstdint>
+#include <cxxabi.h>
 #include <iterator>
-#include <typeinfo>
 #include <sstream>
+#include <typeinfo>
+#include <utility>
+#include <vector>
 
 #include "abg-cxx-compat.h"
 #include "abg-internal.h"
@@ -1301,6 +1302,7 @@ struct elf_symbol::priv
   bool			is_common_;
   bool			is_linux_string_cst_;
   bool			is_in_ksymtab_;
+  uint64_t		crc_;
   bool			is_suppressed_;
   elf_symbol_wptr	main_symbol_;
   elf_symbol_wptr	next_alias_;
@@ -1318,6 +1320,7 @@ struct elf_symbol::priv
       is_common_(false),
       is_linux_string_cst_(false),
       is_in_ksymtab_(false),
+      crc_(0),
       is_suppressed_(false)
   {}
 
@@ -1333,6 +1336,7 @@ struct elf_symbol::priv
        elf_symbol::visibility	  vi,
        bool			  is_linux_string_cst,
        bool			  is_in_ksymtab,
+       uint64_t			  crc,
        bool			  is_suppressed)
     : env_(e),
       index_(i),
@@ -1346,6 +1350,7 @@ struct elf_symbol::priv
       is_common_(c),
       is_linux_string_cst_(is_linux_string_cst),
       is_in_ksymtab_(is_in_ksymtab),
+      crc_(crc),
       is_suppressed_(is_suppressed)
   {
     if (!is_common_)
@@ -1404,6 +1409,7 @@ elf_symbol::elf_symbol(const environment* e,
 		       visibility	  vi,
 		       bool		  is_linux_string_cst,
 		       bool		  is_in_ksymtab,
+		       uint64_t		  crc,
 		       bool		  is_suppressed)
   : priv_(new priv(e,
 		   i,
@@ -1417,6 +1423,7 @@ elf_symbol::elf_symbol(const environment* e,
 		   vi,
 		   is_linux_string_cst,
 		   is_in_ksymtab,
+		   crc,
 		   is_suppressed))
 {}
 
@@ -1476,11 +1483,12 @@ elf_symbol::create(const environment* e,
 		   visibility	      vi,
 		   bool		      is_linux_string_cst,
 		   bool		      is_in_ksymtab,
+		   uint64_t	      crc,
 		   bool		      is_suppressed)
 {
   elf_symbol_sptr sym(new elf_symbol(e, i, s, n, t, b, d, c, ve, vi,
 				     is_linux_string_cst,
-				     is_in_ksymtab, is_suppressed));
+				     is_in_ksymtab, crc, is_suppressed));
   sym->priv_->main_symbol_ = sym;
   return sym;
 }
@@ -1501,7 +1509,9 @@ textually_equals(const elf_symbol&l,
 		 && l.is_public() == r.is_public()
 		 && l.is_defined() == r.is_defined()
 		 && l.is_common_symbol() == r.is_common_symbol()
-		 && l.get_version() == r.get_version());
+		 && l.get_version() == r.get_version()
+		 && (l.get_crc() == 0 || r.get_crc() == 0
+		     || l.get_crc() == r.get_crc()));
 
   if (equals && l.is_variable())
     // These are variable symbols.  Let's compare their symbol size.
@@ -1707,6 +1717,14 @@ elf_symbol::is_in_ksymtab() const
 void
 elf_symbol::set_is_in_ksymtab(bool is_in_ksymtab)
 {priv_->is_in_ksymtab_ = is_in_ksymtab;}
+
+uint64_t
+elf_symbol::get_crc() const
+{return priv_->crc_;}
+
+void
+elf_symbol::set_crc(uint64_t crc)
+{priv_->crc_ = crc;}
 
 bool
 elf_symbol::is_suppressed() const

src/abg-reader.cc
@@ -2835,6 +2835,10 @@ build_elf_symbol(read_context& ctxt, const xmlNodePtr node,
 	is_default_version = true;
     }
 
+  uint64_t crc = 0;
+  if (xml_char_sptr s = XML_NODE_GET_ATTRIBUTE(node, "crc"))
+    crc = strtoull(CHAR_STR(s), NULL, 0);
+
   elf_symbol::type type = elf_symbol::NOTYPE_TYPE;
   read_elf_symbol_type(node, type);
 
@@ -2855,6 +2859,10 @@ build_elf_symbol(read_context& ctxt, const xmlNodePtr node,
 					 is_defined, is_common,
 					 version, visibility,
 					 /*is_linux_string_cst=*/false);
+
+  if (crc != 0)
+    e->set_crc(crc);
+
   return e;
 }
 

src/abg-reporter-priv.cc
@@ -57,8 +57,7 @@ emit_num_value(uint64_t value, const diff_context& ctxt, ostream& out)
     out << std::hex << std::showbase ;
   else
     out << std::dec;
-  out << value;
-  out << std::dec << std::noshowbase;
+  out << value << std::dec << std::noshowbase;
 }
 
 /// Convert a bits value into a byte value if the current diff context
@@ -1190,6 +1189,21 @@ maybe_report_diff_for_symbol(const elf_symbol_sptr&	symbol1,
       out << symbol1->get_version().str()
 	  << " to "
 	  << symbol2->get_version().str();
+
+      reported = true;
+    }
+
+  if (symbol1->get_crc() != 0 && symbol2->get_crc() != 0
+      && symbol1->get_crc() != symbol2->get_crc())
+    {
+      if (reported)
+	out << ",\n" << indent << "its CRC (modversions) changed from ";
+      else
+	out << "\n" << indent << "CRC value (modversions) changed from ";
+
+      out << std::showbase << std::hex
+	  << symbol1->get_crc() << " to " << symbol2->get_crc()
+	  << std::noshowbase << std::dec;
 
       reported = true;
     }

src/abg-symtab-reader.cc
@@ -176,6 +176,7 @@ symtab::load_(Elf*	       elf_handle,
 
   const bool is_kernel = elf_helpers::is_linux_kernel(elf_handle);
   abg_compat::unordered_set<std::string> exported_kernel_symbols;
+  abg_compat::unordered_map<std::string, uint64_t> crc_values;
 
   const bool is_ppc64 = elf_helpers::architecture_is_ppc64(elf_handle);
 
@@ -218,6 +219,13 @@ symtab::load_(Elf*	       elf_handle,
       if (is_kernel && name.rfind("__ksymtab_", 0) == 0)
 	{
 	  ABG_ASSERT(exported_kernel_symbols.insert(name.substr(10)).second);
+	  continue;
+	}
+      if (is_kernel && name.rfind("__crc_", 0) == 0)
+	{
+	  ABG_ASSERT(
+	    crc_values.insert(std::make_pair(name.substr(6), sym->st_value))
+	      .second);
 	  continue;
 	}
 
@@ -317,6 +325,23 @@ symtab::load_(Elf*	       elf_handle,
 	    (*sym_it)->set_is_in_ksymtab(true);
 	}
       has_ksymtab_entries_ = true;
+    }
+
+  // Now add the CRC values
+  for (abg_compat::unordered_map<std::string, uint64_t>::const_iterator
+	 it = crc_values.begin(),
+	 end = crc_values.end();
+       it != end; ++it)
+    {
+      const name_symbol_map_type::const_iterator r =
+	name_symbol_map_.find(it->first);
+      if (r == name_symbol_map_.end())
+	continue;
+
+      for (elf_symbols::const_iterator sym_it = r->second.begin(),
+				       sym_end = r->second.end();
+	   sym_it != sym_end; ++sym_it)
+	(*sym_it)->set_crc(it->second);
     }
 
   // sort the symbols for deterministic output

commit 264fb4e284c32e352b97e2ad8529b05840ab4f59

comp-filter: Consider CRC changes harmful
In case a function or variable symbol changes in a harmless way, we
would previously also consider a CRC change harmless (or more precise:
not harmful).

Fix that by explicitly testing for CRC changes when analyzing whether
diff nodes need to be considered harmful.

	* src/abg-comp-filter.cc (crc_changed): New function.
	  (categorize_harmful_diff_node): Also test for CRC changes.
	* tests/data/Makefile.am: Add new test files.
	* tests/data/test-abidiff-exit/test-crc-report.txt: New test file.
	* tests/data/test-abidiff-exit/test-crc-v0.abi: Likewise.
	* tests/data/test-abidiff-exit/test-crc-v1.abi: Likewise.
	* tests/test-abidiff-exit.cc: Add new test case.

Fixes: 6a56bcb744e8 ("symtab: Add support for MODVERSIONS (CRC checksums)")
Reviewed-by: Giuliano Procida <gprocida@google.com>
Signed-off-by: Matthias Maennich <maennich@google.com>

patches/series
@@ -24,3 +24,4 @@ abg-writer.cc-Track-types-by-plain-address.patch
 KMI-Whitelists-allow-alternative-section-suffix-symbol_list.patch
 dwarf-reader-sort-the-types-for-canonicalization.patch
 abg-writer.cc-fix-write_elf_symbol_reference-loop.patch
+comp-filter-Consider-CRC-changes-harmful.patch

src/abg-comp-filter.cc
@@ -233,6 +233,42 @@ access_changed(const decl_base_sptr& f, const decl_base_sptr& s)
   return false;
 }
 
+/// Test if there was a function or variable CRC change.
+///
+/// @param f the first function or variable to consider.
+///
+/// @param s the second function or variable to consider.
+///
+/// @return true if the test is positive, false otherwise.
+template <typename function_or_var_decl_sptr>
+static bool
+crc_changed(const function_or_var_decl_sptr& f,
+	    const function_or_var_decl_sptr& s)
+{
+  const auto symbol_f  = f->get_symbol(), symbol_s = s->get_symbol();
+  if (!symbol_f || !symbol_s)
+    return false;
+  const auto crc_f = symbol_f->get_crc(), crc_s = symbol_s->get_crc();
+  return (crc_f != 0 && crc_s != 0 && crc_f != crc_s);
+}
+
+/// Test if the current diff tree node carries a CRC change in either a
+/// function or a variable.
+///
+/// @param diff the diff tree node to consider.
+///
+/// @return true if the test is positive, false otherwise.
+static bool
+crc_changed(const diff* diff)
+{
+  if (const function_decl_diff* d =
+	dynamic_cast<const function_decl_diff*>(diff))
+    return crc_changed(d->first_function_decl(), d->second_function_decl());
+  if (const var_diff* d = dynamic_cast<const var_diff*>(diff))
+    return crc_changed(d->first_var(), d->second_var());
+  return false;
+}
+
 /// Test if there was a function name change, but there there was no
 /// change in name of the underlying symbol.  IOW, if the name of a
 /// function changed, but the symbol of the new function is equal to
@@ -1732,7 +1768,8 @@ categorize_harmful_diff_node(diff *d, bool pre)
 	      || non_static_data_member_type_size_changed(f, s)
 	      || non_static_data_member_added_or_removed(d)
 	      || base_classes_added_or_removed(d)
-	      || has_harmful_enum_change(d)))
+	      || has_harmful_enum_change(d)
+	      || crc_changed(d)))
 	category |= SIZE_OR_OFFSET_CHANGE_CATEGORY;
 
       if (has_virtual_mem_fn_change(d))

tests/data/Makefile.am
@@ -191,6 +191,9 @@ test-abidiff-exit/test-headers-dirs/test-headers-dir-v1.o \
 test-abidiff-exit/test-missing-alias-report.txt \
 test-abidiff-exit/test-missing-alias.abi \
 test-abidiff-exit/test-missing-alias.suppr \
+test-abidiff-exit/test-crc-report.txt \
+test-abidiff-exit/test-crc-v0.abi \
+test-abidiff-exit/test-crc-v1.abi \
 \
 test-diff-dwarf/test0-v0.cc		\
 test-diff-dwarf/test0-v0.o			\

tests/data/test-abidiff-exit/test-crc-v0.abi
@@ -1,0 +1,29 @@
+<abi-corpus path='out1.o' architecture='elf-amd-x86_64'>
+  <elf-function-symbols>
+    <elf-symbol name='func1' type='func-type' binding='global-binding' visibility='default-visibility' is-defined='yes' crc='0x10000001'/>
+    <elf-symbol name='func2' type='func-type' binding='global-binding' visibility='default-visibility' is-defined='yes' crc='0x20000001'/>
+  </elf-function-symbols>
+  <elf-variable-symbols>
+    <elf-symbol name='var1' size='4' type='object-type' binding='global-binding' visibility='default-visibility' is-defined='yes' crc='0x30000001'/>
+    <elf-symbol name='var2' size='4' type='object-type' binding='global-binding' visibility='default-visibility' is-defined='yes' crc='0x40000001'/>
+  </elf-variable-symbols>
+  <abi-instr version='1.0' address-size='64' path='test1.c' language='LANG_C99'>
+    <type-decl name='int' size-in-bits='32' id='type-id-1'/>
+    <type-decl name='unnamed-enum-underlying-type' is-anonymous='yes' size-in-bits='32' alignment-in-bits='32' id='type-id-2'/>
+    <type-decl name='void' id='type-id-3'/>
+    <enum-decl name='E' id='type-id-4'>
+      <underlying-type type-id='type-id-2'/>
+      <enumerator name='VALUE_OLD' value='0'/>
+    </enum-decl>
+    <var-decl name='var1' type-id='type-id-1' mangled-name='var1' visibility='default' elf-symbol-id='var1'/>
+    <var-decl name='var2' type-id='type-id-1' mangled-name='var2' visibility='default' elf-symbol-id='var2'/>
+    <function-decl name='func2' mangled-name='func2' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='func2'>
+      <parameter type-id='type-id-4' name='e'/>
+      <return type-id='type-id-3'/>
+    </function-decl>
+    <function-decl name='func1' mangled-name='func1' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='func1'>
+      <parameter type-id='type-id-4' name='e'/>
+      <return type-id='type-id-3'/>
+    </function-decl>
+  </abi-instr>
+</abi-corpus>

tests/data/test-abidiff-exit/test-crc-v1.abi
@@ -1,0 +1,30 @@
+<abi-corpus path='out2.o' architecture='elf-amd-x86_64'>
+  <elf-function-symbols>
+    <elf-symbol name='func1' type='func-type' binding='global-binding' visibility='default-visibility' is-defined='yes' crc='0x10000002'/>
+    <elf-symbol name='func2' type='func-type' binding='global-binding' visibility='default-visibility' is-defined='yes' crc='0x20000001'/>
+  </elf-function-symbols>
+  <elf-variable-symbols>
+    <elf-symbol name='var1' size='4' type='object-type' binding='global-binding' visibility='default-visibility' is-defined='yes' crc='0x30000002'/>
+    <elf-symbol name='var2' size='4' type='object-type' binding='global-binding' visibility='default-visibility' is-defined='yes' crc='0x40000001'/>
+  </elf-variable-symbols>
+  <abi-instr version='1.0' address-size='64' path='test2.c' language='LANG_C99'>
+    <type-decl name='int' size-in-bits='32' id='type-id-1'/>
+    <type-decl name='unnamed-enum-underlying-type' is-anonymous='yes' size-in-bits='32' alignment-in-bits='32' id='type-id-2'/>
+    <type-decl name='void' id='type-id-3'/>
+    <enum-decl name='E' id='type-id-4'>
+      <underlying-type type-id='type-id-2'/>
+      <enumerator name='VALUE_OLD' value='0'/>
+      <enumerator name='VALUE_NEW' value='1'/>
+    </enum-decl>
+    <var-decl name='var1' type-id='type-id-1' mangled-name='var1' visibility='default' elf-symbol-id='var1'/>
+    <var-decl name='var2' type-id='type-id-1' mangled-name='var2' visibility='default' elf-symbol-id='var2'/>
+    <function-decl name='func2' mangled-name='func2' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='func2'>
+      <parameter type-id='type-id-4' name='e'/>
+      <return type-id='type-id-3'/>
+    </function-decl>
+    <function-decl name='func1' mangled-name='func1' visibility='default' binding='global' size-in-bits='64' elf-symbol-id='func1'>
+      <parameter type-id='type-id-4' name='e'/>
+      <return type-id='type-id-3'/>
+    </function-decl>
+  </abi-instr>
+</abi-corpus>

tests/test-abidiff-exit.cc
@@ -374,6 +374,17 @@ InOutSpec in_out_specs[] =
     "data/test-abidiff-exit/test-missing-alias-report.txt",
     "output/test-abidiff-exit/test-missing-alias-report.txt"
   },
+  {
+    "data/test-abidiff-exit/test-crc-v0.abi",
+    "data/test-abidiff-exit/test-crc-v1.abi",
+    "",
+    "",
+    "",
+    "",
+    abigail::tools_utils::ABIDIFF_ABI_CHANGE,
+    "data/test-abidiff-exit/test-crc-report.txt",
+    "output/test-abidiff-exit/test-crc-report.txt"
+  },
   {0, 0, 0 ,0, 0, 0, abigail::tools_utils::ABIDIFF_OK, 0, 0}
 };
 